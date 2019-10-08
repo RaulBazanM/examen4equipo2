5806f47cfbb59ec12eebbd6a980cfba848c87a82
Alejandro Bazan <bazan.0720@gmail.com>
2019-10-08 10:14:00 -0500

    Servicio Compute
    Creacion del servicio compute

---
+++ Boss-Final-Compute/src/main/java/com/everis/compute/BossFinalComputeApplication.java
@@ -0,0 +1,13 @@
+package com.everis.compute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BossFinalComputeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BossFinalComputeApplication.class, args);
+	}
+
+}
