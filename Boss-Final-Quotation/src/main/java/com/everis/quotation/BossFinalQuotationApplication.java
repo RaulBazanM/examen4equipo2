b735e4c31b5f7005035d4e444e59d0ba8d978463
Raul Sanchez Olvera <raul.sanchez.92@ciencias.unam.mx>
2019-10-08 11:33:21 -0500

    Quotation

---
+++ Boss-Final-Quotation/src/main/java/com/everis/quotation/BossFinalQuotationApplication.java
@@ -0,0 +1,13 @@
+package com.everis.quotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BossFinalQuotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BossFinalQuotationApplication.class, args);
+	}
+
+}
