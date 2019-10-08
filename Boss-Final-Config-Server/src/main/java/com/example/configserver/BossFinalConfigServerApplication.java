ad5f01bd616dfdadfe2b6a24c5f24bb981697475
Adrian <adriansalgado215@gmail.com>
2019-10-08 10:54:08 -0500

    Se cambio el puerto de config server a 8080

--- Boss-Final-Config-Server/src/main/java/com/example/configserver/BossFinalConfigServerApplication.java
+++ Boss-Final-Config-Server/src/main/java/com/example/configserver/BossFinalConfigServerApplication.java
@@ -3,13 +3,14 @@ package com.example.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class BossFinalConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BossFinalConfigServerApplication.class, args);
 	}
-
 }
